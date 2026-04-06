4d32c6c0431f3e18b2ce9d9b8b62325d12eaacb5
epoh-amd <ErnQi.Poh@amd.com>
2026-04-06 23:21:45 +0800

    wew

--- routes/email.js
+++ routes/email.js
@@ -36,7 +36,7 @@ const sendCombinedDashboardEmail = async (html, attachments, recipients) => {
 
 
 
-cron.schedule('0 23 * * *', async () => {
+cron.schedule('30 23 * * *', async () => {
   console.log('Running combined dashboard cron...');
   try {
     const recipients = process.env.EMAIL_RECIPIENTS

5ca860c5ba5f7fad4e60a64384e6dcea1387dd6e
epoh <ErnQi.Poh@amd.com>
2026-08-17 04:00:22 +0800

    8/17

--- routes/email.js
+++ routes/email.js
@@ -828,7 +828,7 @@ router.post('/waiver/requestor-notify', async (req, res) => {
   }
 
   const notifierEmails = await getNotifierEmails();
-  const defaultCcEmails = [''];
+  const defaultCcEmails = ['LayLing.Chew@amd.com', 'Amanda.KoayBeeWah@amd.com'];
   const toSet = new Set(recipientEmails.map(e => e.toLowerCase()));
   const allCcRequestor = [...new Set([...(ccEmail ? [ccEmail] : []), ...notifierEmails, ...defaultCcEmails].filter(e => !toSet.has(e.toLowerCase())))];
 
